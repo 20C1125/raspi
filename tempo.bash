00f8451964a808bbe9171f495ae58a715299854e
Masamitsu Yamazaki <s20c1125rk@s.chibakoudai.jp>
2021-12-09 21:20:39 +0000

    first

---
+++ tempo.bash
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo 何回点灯させますか？
+read not
+echo 何秒で切り替えますか？
+read speed
+count=0	
+
+	while [ $count != $not ]
+	do
+		echo 1 > /dev/myled0
+		sleep $speed
+		echo 0 > /dev/myled0
+		sleep $speed
+		count=`expr 1 + $count`
+		echo $count回
+	done
