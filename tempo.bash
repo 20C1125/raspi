9790035e82e694017fe9cf9b6896719382797287
Masamitsu Yamazaki <s20c1125rk@s.chibakoudai.jp>
2021-12-14 19:35:15 +0000

    Update

--- tempo.bash
+++ tempo.bash
@@ -9,8 +9,10 @@ count=0
 	while [ $count != $not ]
 	do
 		echo 1 > /dev/myled0
+		echo 2 > /dev/myled0
 		sleep $speed
 		echo 0 > /dev/myled0
+		echo 3 > /dev/myled0
 		sleep $speed
 		count=`expr 1 + $count`
 		echo $count回
